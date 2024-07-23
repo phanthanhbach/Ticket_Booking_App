31e62ed6847f1fe494c44778176ded5a72e303f0
Phan Thanh Bách <phanbach02@gmail.com>
2024-07-23 19:16:27 +0700

    Create genres.ts

---
+++ src/constants/genres.ts
@@ -0,0 +1,43 @@
+interface Genres {
+    28: string;
+    12: string;
+    16: string;
+    35: string;
+    80: string;
+    99: string;
+    18: string;
+    10751: string;
+    14: string;
+    36: string;
+    27: string;
+    10402: string;
+    9648: string;
+    10749: string;
+    878: string;
+    10770: string;
+    53: string;
+    10752: string;
+    37: string;
+}
+
+export const genres: Genres = {
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Documentry',
+    18: 'Drama',
+    10751: 'Family',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystry',
+    10749: 'Romance',
+    878: 'Science Fiction',
+    10770: 'TV Movie',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western',
+};
